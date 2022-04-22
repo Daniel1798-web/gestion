50c8889c23e2903088126ba1bea8d1c426e762a5
Daniel1798-web <danrieloto@gmail.com>
2022-04-22 18:14:59 -0300

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './index.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
